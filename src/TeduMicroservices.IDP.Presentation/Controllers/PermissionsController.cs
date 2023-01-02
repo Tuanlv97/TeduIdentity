5178cb34911e332db4160cde12edb5a7adaefdde
anhtuan37 <anhtuan20011997@gmail.com>
2023-01-02 16:16:23 +0700

    Add Store procedure

--- src/TeduMicroservices.IDP.Presentation/Controllers/PermissionsController.cs
+++ src/TeduMicroservices.IDP.Presentation/Controllers/PermissionsController.cs
@@ -1,20 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Net;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using TeduMicroservices.IDP.Infrastructure.Repositories;
 using TeduMicroservices.IDP.Infrastructure.ViewModels;
 
 namespace TeduMicroservices.IDP.Presentation.Controllers;
 
-
 [ApiController]
 [Route("api/[controller]/roles/{roleId}")]
+[Authorize("Bearer")]
 public class PermissionsController : ControllerBase
 {
     private readonly IRepositoryManager _repository;
 
     public PermissionsController(IRepositoryManager repository)
     {
-        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        _repository = repository;
     }
 
     [HttpGet]
@@ -23,5 +25,29 @@ public class PermissionsController : ControllerBase
     {
         var result = await _repository.Permission.GetPermissionsByRole(roleId);
         return Ok(result);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(PermissionViewModel), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> CreatePermission(string roleId, [FromBody] PermissionAddModel model)
+    {
+        var result = await _repository.Permission.CreatePermission(roleId, model);
+        return result != null ? Ok(result) : NoContent();
+    }
+
+    [HttpDelete("function/{function}/command/{command}")]
+    [ProducesResponseType(typeof(PermissionViewModel), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> DeletePermission(string roleId, [Required] string function, [Required] string command)
+    {
+        await _repository.Permission.DeletePermission(roleId, function, command);
+        return NoContent();
+    }
+
+    [HttpPost("update-permissions")]
+    [ProducesResponseType(typeof(NoContentResult), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> UpdatePermissions(string roleId, [FromBody] IEnumerable<PermissionAddModel> permissions)
+    {
+        await _repository.Permission.UpdatePermissionsByRoleId(roleId, permissions);
+        return NoContent();
     }
 }
